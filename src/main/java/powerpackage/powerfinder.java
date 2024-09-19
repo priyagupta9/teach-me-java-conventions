6a3cc35cf2de1ada59668f1c5aa115489db90032
Priya Gupta <guptaa.priya9@gmail.com>
2024-09-19 22:09:17 +0530

    Update powerfinder.java

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,11 +1,15 @@
 package powerpackage;
 
-public class powerfinder {
-    public static int OF(int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
+// Changing class name to PascalCase Convention
+public class PowerFinder {
+    // Changing method name to camelCase Convention
+    // updating param names to meaningful name
+    public static int findPow(int base, int exponent) {
+        // chnging var name to meaningful name
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
         }
-        return p;
+        return result;
     }
 }
