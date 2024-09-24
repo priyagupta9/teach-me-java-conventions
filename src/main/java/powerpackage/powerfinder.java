1bc555dec55ce02fa1ef15e1d4edf3f59f708c63
Priya Gupta <guptaa.priya9@gmail.com>
2024-09-24 09:47:47 +0530

    Update powerfinder.java

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -2,10 +2,10 @@ package powerpackage;
 
 public class powerfinder {
     public static int OF(int xyz, int pqr) {
-        int p = 1;
+        int result = 1;
         for (int i = 0; i<pqr; i++) {
-            p *= xyz;
+            result *= xyz;
         }
-        return p;
+        return result;
     }
 }
